7b0425e8097223e0cb74863c4c578fb6d058fe6e
Andres_Alvarez <andres.alvarez094@hotmail.es>
2024-01-21 19:34:57 -0500

    Creacion de los equipos en la tabla principal con sus respectivos valores iniciales

--- index.py
+++ index.py
@@ -37,21 +37,30 @@ def main():
                 visitante = input(f'Equipo visitante: ').lower()
                 gol_visitante = int(input('Goles: '))
 
+                equipos_en_partido.append([local, visitante])
 
                 # TODO: Validar la existencia de los equipos en la tabla de posiciones
                 # Obtener el nombre de los equipos
                 equiposExistentes = []
                 for equipo in range(len(tablaEquipos)):
                     equiposExistentes.append(tablaEquipos[equipo][0])
-                
+
                 # validar existencia de los equipos en partido en la tabla general de los equipos
                 for equipo in range(len(equipos_en_partido)):
                     if equipos_en_partido[equipo] in equiposExistentes:
                         # actualice datos
                         pass
                     else:
-                        # cree al equipo
-                        pass
+                        # cree al equipo con valores iniciales segun marcador
+                        if gol_local > gol_visitante:
+                            tablaEquipos.append([local, 1, 1, 0, 0, gol_local, gol_visitante, 3])
+                            tablaEquipos.append([visitante, 1, 0, 1, 0, gol_visitante, gol_local, 0])
+                        elif gol_local < gol_visitante:
+                            tablaEquipos.append([local, 1, 0, 1, 0, gol_local, gol_visitante, 0])
+                            tablaEquipos.append([visitante, 1, 1, 0, 0, gol_visitante, gol_local, 3])
+                        else:
+                            tablaEquipos.append([local, 1, 0, 0, 1, gol_local, gol_visitante, 1])
+                            tablaEquipos.append([visitante, 1, 0, 0, 1, gol_visitante, gol_local, 1])
 
                 agregarPartido = input('Agregar otro partido [Y/N]: ')
                 if agregarPartido.lower() == 'y':
@@ -78,6 +87,8 @@ def main():
         pass
     else:
         pass
+
+    print(tablaEquipos)
 
 
 if __name__ == '__main__':
